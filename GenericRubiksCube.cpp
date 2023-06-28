ccea8334671e361d5f004e4f01c0148def2fdd88
utsavdhanuka03 <utsavdhanuka03@kgpian.iitkgp.ac.in>
2023-06-28 20:46:52 +0530

    Added Some initial files

--- GenericRubiksCube.cpp
+++ GenericRubiksCube.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "GenericRubiksCube.h"
-
+using namespace std;
 //
 //Given a color return it's first letter
 //
@@ -65,3 +65,243 @@ string GenericRubiksCube::getMove(GenericRubiksCube::MOVE ind) {
 /*
  * Perform a move operation on using a Move index.
  */
+GenericRubiksCube &GenericRubiksCube::move(GenericRubiksCube::MOVE ind) {
+    if (ind == MOVE::L) {
+        return this->l();
+    } else if (ind == MOVE::LPRIME) {
+        return this->lPrime();
+    } else if (ind == MOVE::L2) {
+        return this->l2();
+    } else if (ind == MOVE::R) {
+        return this->r();
+    } else if (ind == MOVE::RPRIME) {
+        return this->rPrime();
+    } else if (ind == MOVE::R2) {
+        return this->r2();
+    } else if (ind == MOVE::U) {
+        return this->u();
+    } else if (ind == MOVE::UPRIME) {
+        return this->uPrime();
+    } else if (ind == MOVE::U2) {
+        return this->u2();
+    } else if (ind == MOVE::D) {
+        return this->d();
+    } else if (ind == MOVE::DPRIME) {
+        return this->dPrime();
+    } else if (ind == MOVE::D2) {
+        return this->d2();
+    } else if (ind == MOVE::F) {
+        return this->f();
+    } else if (ind == MOVE::FPRIME) {
+        return this->fPrime();
+    } else if (ind == MOVE::F2) {
+        return this->f2();
+    } else if (ind == MOVE::B) {
+        return this->b();
+    } else if (ind == MOVE::BPRIME) {
+        return this->bPrime();
+    } else if (ind == MOVE::B2) {
+        return this->b2();
+    }
+}
+/*
+ * Invert a move.
+ */
+GenericRubiksCube &GenericRubiksCube::invert(GenericRubiksCube::MOVE ind) {
+    if (ind == MOVE::L) {
+        return this->l();
+    } else if (ind == MOVE::LPRIME) {
+        return this->lPrime();
+    } else if (ind == MOVE::L2) {
+        return this->l2();
+    } else if (ind == MOVE::R) {
+        return this->r();
+    } else if (ind == MOVE::RPRIME) {
+        return this->rPrime();
+    } else if (ind == MOVE::R2) {
+        return this->r2();
+    } else if (ind == MOVE::U) {
+        return this->u();
+    } else if (ind == MOVE::UPRIME) {
+        return this->uPrime();
+    } else if (ind == MOVE::U2) {
+        return this->u2();
+    } else if (ind == MOVE::D) {
+        return this->d();
+    } else if (ind == MOVE::DPRIME) {
+        return this->dPrime();
+    } else if (ind == MOVE::D2) {
+        return this->d2();
+    } else if (ind == MOVE::F) {
+        return this->f();
+    } else if (ind == MOVE::FPRIME) {
+        return this->fPrime();
+    } else if (ind == MOVE::F2) {
+        return this->f2();
+    } else if (ind == MOVE::B) {
+        return this->b();
+    } else if (ind == MOVE::BPRIME) {
+        return this->bPrime();
+    } else if (ind == MOVE::B2) {
+        return this->b2();
+    }
+}
+ void GenericRubiksCube::print() const {
+     cout << "Rubik's Cube:\n\n";
+
+     for (int row = 0; row <= 2; row++) {
+         for (unsigned i = 0; i < 7; i++) cout << " ";
+         for (int col = 0; col <= 2; col++) {
+             cout << getColorLetter(getColor(FACE::UP, row, col)) << " ";
+         }
+         cout << "\n";
+     }
+     cout<<"\n";
+
+     for (int row = 0; row <= 2; row++) {
+
+         for (int col = 0; col <= 2; col++) {
+             cout << getColorLetter(getColor(FACE::LEFT, row, col)) << " ";
+         }
+         cout << " ";
+
+         for (int col = 0; col <= 2; col++) {
+             cout << getColorLetter(getColor(FACE::FRONT, row, col)) << " ";
+         }
+         cout << " ";
+
+         for (int col = 0; col <= 2; col++) {
+             cout << getColorLetter(getColor(FACE::RIGHT, row, col)) << " ";
+         }
+         cout << " ";
+
+         for (int col = 0; col <= 2; col++) {
+             cout << getColorLetter(getColor(FACE::BACK, row, col)) << " ";
+         }
+         cout << "\n";
+     }
+
+     cout << "\n";
+
+     for (int row = 0; row <= 2; row++) {
+         for (unsigned i = 0; i < 7; i++) cout << " ";
+         for (int col = 0; col <= 2; col++) {
+             cout << getColorLetter(getColor(FACE::DOWN, row, col)) << " ";
+         }
+         cout << "\n";
+     }
+     cout << "\n";
+ }
+ vector<GenericRubiksCube::MOVE> GenericRubiksCube::randomShuffleCube(unsigned int times) {
+     vector<MOVE> moves_performed;
+     srand(time(0));
+     for (unsigned int i = 0; i < times; i++) {
+         unsigned int selectMove = (rand() % 18);
+         moves_performed.push_back(static_cast<MOVE>(selectMove));
+         this->move(static_cast<MOVE>(selectMove));
+     }
+     return moves_performed;
+}
+string GenericRubiksCube::getCornerColorString(uint8_t ind) const {
+    string str="";
+    switch (ind) {
+//        UFR
+        case 0:
+            str += getColorLetter(getColor(FACE::UP, 2, 2));
+            str += getColorLetter(getColor(FACE::FRONT, 0, 2));
+            str += getColorLetter(getColor(FACE::RIGHT, 0, 0));
+            break;
+
+//            UFL
+        case 1:
+            str += getColorLetter(getColor(FACE::UP, 2, 0));
+            str += getColorLetter(getColor(FACE::FRONT, 0, 0));
+            str += getColorLetter(getColor(FACE::LEFT, 0, 2));
+            break;
+
+//            UBL
+        case 2:
+            str += getColorLetter(getColor(FACE::UP, 0, 0));
+            str += getColorLetter(getColor(FACE::BACK, 0, 2));
+            str += getColorLetter(getColor(FACE::LEFT, 0, 0));
+            break;
+
+//            UBR
+        case 3:
+            str += getColorLetter(getColor(FACE::UP, 0, 2));
+            str += getColorLetter(getColor(FACE::BACK, 0, 0));
+            str += getColorLetter(getColor(FACE::RIGHT, 0, 2));
+            break;
+
+//            DFR
+        case 4:
+            str += getColorLetter(getColor(FACE::DOWN, 0, 2));
+            str += getColorLetter(getColor(FACE::FRONT, 2, 2));
+            str += getColorLetter(getColor(FACE::RIGHT, 2, 0));
+            break;
+
+//            DFL
+        case 5:
+            str += getColorLetter(getColor(FACE::DOWN, 0, 0));
+            str += getColorLetter(getColor(FACE::FRONT, 2, 0));
+            str += getColorLetter(getColor(FACE::LEFT, 2, 2));
+            break;
+
+//            DBR
+        case 6:
+            str += getColorLetter(getColor(FACE::DOWN, 2, 2));
+            str += getColorLetter(getColor(FACE::BACK, 2, 0));
+            str += getColorLetter(getColor(FACE::RIGHT, 2, 2));
+            break;
+
+//            DBL
+        case 7:
+            str += getColorLetter(getColor(FACE::DOWN, 2, 0));
+            str += getColorLetter(getColor(FACE::BACK, 2, 2));
+            str += getColorLetter(getColor(FACE::LEFT, 2, 0));
+            break;
+    }
+    return str;
+}
+uint8_t GenericRubiksCube::getCornerIndex(uint8_t ind) const {
+    string corner = getCornerColorString(ind);
+
+    uint8_t ret = 0;
+    for (auto c: corner) {
+        if (c != 'W' && c != 'Y') continue;
+        if (c == 'Y') {
+            ret |= (1 << 2);
+        }
+    }
+
+    for (auto c: corner) {
+        if (c != 'R' && c != 'O') continue;
+        if (c == 'O') {
+            ret |= (1 << 1);
+        }
+    }
+
+    for (auto c: corner) {
+        if (c != 'B' && c != 'G') continue;
+        if (c == 'G') {
+            ret |= (1 << 0);
+        }
+    }
+    return ret;
+}
+uint8_t GenericRubiksCube::getCornerOrientation(uint8_t ind) const {
+    string corner = getCornerColorString(ind);
+
+    string actual_str = "";
+
+    for (auto c: corner) {
+        if (c != 'W' && c != 'Y') continue;
+        actual_str.push_back(c);
+    }
+
+    if (corner[1] == actual_str[0]) {
+        return 1;
+    } else if (corner[2] == actual_str[0]) {
+        return 2;
+    } else return 0;
+}
--- GenericRubiksCube.h
+++ GenericRubiksCube.h
@@ -149,6 +149,11 @@ class GenericRubiksCube { // defining enums for notations
 
     virtual GenericRubiksCube &b2() = 0;
 
+ string getCornerColorString(uint8_t ind) const;
+
+ uint8_t getCornerIndex(uint8_t ind) const;
+
+ uint8_t getCornerOrientation(uint8_t ind) const;
 };
 
 
