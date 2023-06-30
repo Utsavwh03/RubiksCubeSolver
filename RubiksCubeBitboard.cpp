c12e84f8d3d6cd9f742ecf62c03fca2a36ddccca
utsavdhanuka03 <utsavdhanuka03@kgpian.iitkgp.ac.in>
2023-06-30 15:31:15 +0530

    Added Some initial files

--- RubiksCubeBitboard.cpp
+++ RubiksCubeBitboard.cpp
@@ -16,6 +16,16 @@
          side=side>>48;
          bitboard[ind]=(bitboard[ind]<<16)|(side);
      }
+     void rotateSide(int s1, int s1_1, int s1_2, int s1_3, int s2, int s2_1, int s2_2, int s2_3) {
+         uint64_t clr1 = (bitboard[s2] & (one_8 << (8 * s2_1))) >> (8 * s2_1);
+         uint64_t clr2 = (bitboard[s2] & (one_8 << (8 * s2_2))) >> (8 * s2_2);
+         uint64_t clr3 = (bitboard[s2] & (one_8 << (8 * s2_3))) >> (8 * s2_3);
+
+         bitboard[s1] = (bitboard[s1] & ~(one_8 << (8 * s1_1))) | (clr1 << (8 * s1_1));
+         bitboard[s1] = (bitboard[s1] & ~(one_8 << (8 * s1_2))) | (clr2 << (8 * s1_2));
+         bitboard[s1] = (bitboard[s1] & ~(one_8 << (8 * s1_3))) | (clr3 << (8 * s1_3));
+     }
+
 
 
  public:
@@ -53,5 +63,191 @@
          }
          return true;
      }
+     GenericRubiksCube &u() override {
+         this->rotateFace(0);
+         uint64_t temp = bitboard[2] & one_24;
+         bitboard[2] = (bitboard[2] & ~one_24) | (bitboard[3] & one_24);
+         bitboard[3] = (bitboard[3] & ~one_24) | (bitboard[4] & one_24);
+         bitboard[4] = (bitboard[4] & ~one_24) | (bitboard[1] & one_24);
+         bitboard[1] = (bitboard[1] & ~one_24) | temp;
 
- };
+         return *this;
+     }
+
+     GenericRubiksCube &uPrime() override {
+         this->u();
+         this->u();
+         this->u();
+
+         return *this;
+     };
+
+     GenericRubiksCube &u2() override {
+         this->u();
+         this->u();
+
+         return *this;
+     }
+     GenericRubiksCube &l() override {
+         this->rotateFace(1);
+         uint64_t clr1 = (bitboard[2] & (one_8 << (8 * 0))) >> (8 * 0);
+         uint64_t clr2 = (bitboard[2] & (one_8 << (8 * 6))) >> (8 * 6);
+         uint64_t clr3 = (bitboard[2] & (one_8 << (8 * 7))) >> (8 * 7);
+
+         this->rotateSide(2, 0, 7, 6, 0, 0, 7, 6);
+         this->rotateSide(0, 0, 7, 6, 4, 4, 3, 2);
+         this->rotateSide(4, 4, 3, 2, 5, 0, 7, 6);
+
+         bitboard[5] = (bitboard[5] & ~(one_8 << (8 * 0))) | (clr1 << (8 * 0));
+         bitboard[5] = (bitboard[5] & ~(one_8 << (8 * 6))) | (clr2 << (8 * 6));
+         bitboard[5] = (bitboard[5] & ~(one_8 << (8 * 7))) | (clr3 << (8 * 7));
+
+         return *this;
+
+     }
+     GenericRubiksCube &lPrime() override {
+         this->l();
+         this->l();
+         this->l();
+
+         return *this;
+     }
+
+     GenericRubiksCube &l2() override {
+         this->l();
+         this->l();
+
+         return *this;
+     }
+     GenericRubiksCube &f() override {
+         this->rotateFace(2);
+
+         uint64_t clr1 = (bitboard[0] & (one_8 << (8 * 4))) >> (8 * 4);
+         uint64_t clr2 = (bitboard[0] & (one_8 << (8 * 5))) >> (8 * 5);
+         uint64_t clr3 = (bitboard[0] & (one_8 << (8 * 6))) >> (8 * 6);
+
+         this->rotateSide(0, 4, 5, 6, 1, 2, 3, 4);
+         this->rotateSide(1, 2, 3, 4, 5, 0, 1, 2);
+         this->rotateSide(5, 0, 1, 2, 3, 6, 7, 0);
+
+         bitboard[3] = (bitboard[3] & ~(one_8 << (8 * 6))) | (clr1 << (8 * 6));
+         bitboard[3] = (bitboard[3] & ~(one_8 << (8 * 7))) | (clr2 << (8 * 7));
+         bitboard[3] = (bitboard[3] & ~(one_8 << (8 * 0))) | (clr3 << (8 * 0));
+
+         return *this;}
+     GenericRubiksCube &fPrime() override {
+         this->f();
+         this->f();
+         this->f();
+         return *this;
+     }
+     GenericRubiksCube &f2() override {
+         this->f();
+         this->f();
+
+         return *this;
+     }
+     GenericRubiksCube &r() override {
+         this->rotateFace(3);
+         uint64_t clr1 = (bitboard[0] & (one_8 << (8 * 2))) >> (8 * 2);
+         uint64_t clr2 = (bitboard[0] & (one_8 << (8 * 3))) >> (8 * 3);
+         uint64_t clr3 = (bitboard[0] & (one_8 << (8 * 4))) >> (8 * 4);
+
+         this->rotateSide(0, 2, 3, 4, 2, 2, 3, 4);
+         this->rotateSide(2, 2, 3, 4, 5, 2, 3, 4);
+         this->rotateSide(5, 2, 3, 4, 4, 7, 6, 0);
+
+         bitboard[4] = (bitboard[4] & ~(one_8 << (8 * 7))) | (clr1 << (8 * 7));
+         bitboard[4] = (bitboard[4] & ~(one_8 << (8 * 6))) | (clr2 << (8 * 6));
+         bitboard[4] = (bitboard[4] & ~(one_8 << (8 * 0))) | (clr3 << (8 * 0));
+
+         return *this;
+     }
+     GenericRubiksCube &rPrime() override {
+         this->r();
+         this->r();
+         this->r();
+
+         return *this;
+     }
+     GenericRubiksCube &r2() override {
+         this->r();
+         this->r();
+
+         return *this;
+     }
+
+     GenericRubiksCube &b() override {
+         this->rotateFace(4);
+
+         uint64_t clr1 = (bitboard[0] & (one_8 << (8 * 0))) >> (8 * 0);
+         uint64_t clr2 = (bitboard[0] & (one_8 << (8 * 1))) >> (8 * 1);
+         uint64_t clr3 = (bitboard[0] & (one_8 << (8 * 2))) >> (8 * 2);
+
+         this->rotateSide(0, 0, 1, 2, 3, 2, 3, 4);
+         this->rotateSide(3, 2, 3, 4, 5, 4, 5, 6);
+         this->rotateSide(5, 4, 5, 6, 1, 6, 7, 0);
+
+         bitboard[1] = (bitboard[1] & ~(one_8 << (8 * 6))) | (clr1 << (8 * 6));
+         bitboard[1] = (bitboard[1] & ~(one_8 << (8 * 7))) | (clr2 << (8 * 7));
+         bitboard[1] = (bitboard[1] & ~(one_8 << (8 * 0))) | (clr3 << (8 * 0));
+
+         return *this;
+     }
+     GenericRubiksCube &bPrime() override {
+         this->b();
+         this->b();
+         this->b();
+
+         return *this;
+     }
+     GenericRubiksCube &b2() override {
+         this->b();
+         this->b();
+
+         return *this;
+     }
+     GenericRubiksCube &d() override {
+         this->rotateFace(5);
+
+         uint64_t clr1 = (bitboard[2] & (one_8 << (8 * 4))) >> (8 * 4);
+         uint64_t clr2 = (bitboard[2] & (one_8 << (8 * 5))) >> (8 * 5);
+         uint64_t clr3 = (bitboard[2] & (one_8 << (8 * 6))) >> (8 * 6);
+
+         this->rotateSide(2, 4, 5, 6, 1, 4, 5, 6);
+         this->rotateSide(1, 4, 5, 6, 4, 4, 5, 6);
+         this->rotateSide(4, 4, 5, 6, 3, 4, 5, 6);
+
+         bitboard[3] = (bitboard[3] & ~(one_8 << (8 * 4))) | (clr1 << (8 * 4));
+         bitboard[3] = (bitboard[3] & ~(one_8 << (8 * 5))) | (clr2 << (8 * 5));
+         bitboard[3] = (bitboard[3] & ~(one_8 << (8 * 6))) | (clr3 << (8 * 6));
+
+         return *this;
+     }
+     GenericRubiksCube &dPrime() override {
+         this->d();
+         this->d();
+         this->d();
+
+         return *this;
+     }
+
+     GenericRubiksCube &d2() override {
+         this->d();
+         this->d();
+
+         return *this;
+     }
+     bool operator==(const RubiksCubeBitboard &r1) const {
+         for (int i = 0; i < 6; i++) {
+             if (this->bitboard[i] != r1.bitboard[i]) return false;
+         }
+         return true;
+     }
+     RubiksCubeBitboard &operator=(const RubiksCubeBitboard &r1) {
+         for (int i = 0; i < 6; i++) {
+             bitboard[i] = r1.bitboard[i];
+         }
+         return *this;
+     }
+     };
